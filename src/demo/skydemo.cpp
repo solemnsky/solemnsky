0b26797752bb2e4a2f89764e0d711a1de819fa89
Chris Gadzinski <zenmags@gmail.com>
2016-01-03 18:50:48 +0000

    sprite sheets working correctly

--- src/demo/skydemo.cpp
+++ src/demo/skydemo.cpp
@@ -12,12 +12,13 @@ sf::Texture getBackground() {
 SkyDemo::SkyDemo() : background(getBackground()) { }
 
 void SkyDemo::tick(float delta) {
+  if (animTicker.tick(delta)) animVal = (animVal + 1) % sheet.count;
   sky.tick(delta);
 }
 
 void SkyDemo::render(ui::Frame &f) {
   f.drawSprite(background, sf::Vector2f(0, 0), sf::IntRect(0, 0, 1600, 900));
-  sheet.drawAt(f, sf::Vector2f(800, 450), sf::Vector2f(100, 100), 0);
+  sheet.drawIndex(f, sf::Vector2f(800, 450), sf::Vector2f(200, 200), animVal);
 
 //  sky.render(sf::Vector2f(0, 0));
 }
--- src/ui/sheet.cpp
+++ src/ui/sheet.cpp
@@ -1,18 +1,17 @@
 #include "sheet.h"
 
 namespace ui {
-void SpriteSheet::drawAt(ui::Frame &f, const sf::Vector2f pos,
-                         const sf::Vector2f dims, const int index) const {
-  const int yShift = index / record.countX;
-  const int xShift = index % record.countX;
+void SpriteSheet::drawIndex(ui::Frame &f, const sf::Vector2f pos,
+                            const sf::Vector2f dims, const int index) const {
+  const int yShift(index % record.countY), xShift(index / record.countY);
   const float spriteWidth(record.tileX), spriteHeight(record.tileY);
+  const int left(xShift * record.tileX), top(yShift * record.tileY);
 
-  f.pushTransform(sf::Transform().scale(dims.x / spriteWidth, dims.y / spriteHeight)
-                      .translate(pos));
-//  f.drawSprite(texture, {-spriteWidth / 2, -spriteHeight / 2},
-//               sf::IntRect(xShift * record.tileX, yShift * record.tileY,
-//                           record.tileX, record.tileY));
-  f.drawSprite(texture, {0, 0}, {0, 0, 100, 100});
+  f.pushTransform(sf::Transform().translate(pos).
+      scale(dims.x / spriteWidth, dims.y / spriteHeight));
+  f.drawSprite(texture, {-spriteWidth / 2, -spriteHeight / 2},
+               sf::IntRect(left, top, record.tileX, record.tileY));
   f.popTransform();
 }
+
 }
