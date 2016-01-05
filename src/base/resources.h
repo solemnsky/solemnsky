65afa1c9106da633d1270afc9d1ef8a9020c23cd
Chris Gadzinski <zenmags@gmail.com>
2016-01-05 14:34:44 +0000

    resource caching

--- src/base/resources.h
+++ src/base/resources.h
@@ -1,11 +1,16 @@
 /**
- * Filepaths / metadata for our resources.
+ * Metadata and caching for our resources.
  */
 #ifndef SOLEMNSKY_RESOURCES_H
 #define SOLEMNSKY_RESOURCES_H
 
 #include <string>
 #include <vector>
+#include <SFML/Graphics.hpp>
+
+/****
+ * Resource metadata.
+ */
 
 enum class Res {
   Font,
@@ -14,21 +19,57 @@ enum class Res {
   LAST // for checking number of resources defined
 };
 
+enum class ResType {
+  Font, Texture
+};
+
 struct ResRecord {
   std::string path;
-  bool isSprite;
+  ResType type;
+
+  // if it's a spritesheet, we have some metadata about how it's laid out
+  bool isSheet;
   int tileX, tileY, countX, countY;
 
   ResRecord(std::string path,
-            bool isSprite,
+            ResType type,
+            bool isSheet,
             int tileX = 0, int tileY = 0, int countX = 0, int countY = 0) :
       path(path),
-      isSprite(isSprite),
+      type(type),
+      isSheet(isSheet),
       tileX(tileX), tileY(tileY), countX(countX), countY(countY) { }
 };
 
 std::string filepathTo(Res res);
 ResRecord recordOf(Res res);
 
+/****
+ * Resource caching.
+ */
+
+namespace detail {
+
+static class ResMan {
+private:
+  std::map<int, sf::Texture> textures;
+  std::map<int, sf::Font> fonts;
+
+public:
+  void loadRes();
+
+  sf::Texture recallTexture(Res res);
+  sf::Font recallFont(Res res);
+} resMan;
+
+}
+
+inline void loadResources() { detail::resMan.loadRes(); }
+
+inline sf::Texture textureFrom(Res res) {
+  return detail::resMan.recallTexture(res);
+}
+
+inline sf::Font fontFrom(Res res) { return detail::resMan.recallFont(res); }
 
 #endif //SOLEMNSKY_RESOURCES_H
--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,12 @@
 #include <iostream>
 #include "ui/control.h"
 #include "demo/demo.h"
+#include "base/resources.h"
 
 #include <stdio.h>
 
 int main(int, char **) {
+  loadResources();
+
   runSFML(std::make_shared<Demo>());
 }
