c5d18098597c3994882f87a24897789825038cad
Chris Gadzinski <zenmags@gmail.com>
2016-01-08 15:29:05 +0000

    with a debug draw, it becomes evident that this is a holy bug-ness bugginess of bugs

--- src/sky/render.cpp
+++ src/sky/render.cpp
@@ -10,8 +10,10 @@ RenderMan::RenderMan(Sky *sky) :
 void RenderMan::render(ui::Frame &f, sf::Vector2f pos) {
   if (Plane *plane = sky->getPlane(0)) {
     if (auto &state = plane->state) {
+      const auto &hitbox = state->tuning.hitbox;
       f.pushTransform(
           sf::Transform().translate(state->pos).rotate(state->rot));
+      f.drawRect(-0.5f * hitbox, 0.5f * hitbox, sf::Color::Red);
       sheet.drawIndex(f, sf::Vector2f(200, 200), 7);
       f.popTransform();
     } else {
