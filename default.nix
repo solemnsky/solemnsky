7560ab9938589b9dd490d235828db42843fc4902
Chris Gadzinski <zenmags@gmail.com>
2016-01-17 20:30:56 +0000

    works on linux now

--- default.nix
+++ default.nix
@@ -3,13 +3,17 @@
 with nixpkgs;
 
 stdenv.mkDerivation {
-  # phases = "patchPhase installPhase";
-
   patchPhase = ''
-  rm -rf thirdparty/Box2D/Box2D/{glew,glfw,HelloWorld,Testbed}/*
+  # rm -rf thirdparty/Box2D/Box2D/{glew,glfw,HelloWorld,Testbed}/*
+  '';
+  
+  buildInputs = [cmake mesa xorg.libX11 xorg.xcbutilimage eudev libjpeg openal flac libogg freetype libvorbis glew glfw2 boost];
+
+  installPhase = ''
+    mkdir $out
+    cp {libsolemnsky.so,solemnsky_client,solemnsky_server} $out
   '';
 
-  buildInputs = [cmake mesa xorg.libX11 xorg.xcbutilimage eudev libjpeg openal flac libogg freetype libvorbis glew glfw2];
   name = "solemnsky";
   src = ./.;
 }
