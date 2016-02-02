f22371b925352f7d0dafe050f83b8ab9c92faa88
MagneticDuck <zenmags@gmail.com>
2016-02-02 22:13:37 +0000

    packing stuff, hopefully 100% less memory corruption this time

--- src/pack/pack.h
+++ src/pack/pack.h
@@ -6,6 +6,7 @@
 
 #include "packet.h"
 #include <functional>
+#include <string>
 
 namespace pk {
 
@@ -17,36 +18,37 @@ namespace pk {
 template<typename Value>
 struct Pack {
   Pack() = delete;
+
   Pack(std::function<void(Packet &, const Value &)> pack,
        std::function<void(Packet &, Value &)> unpack) :
-      pack(pack), unpack(unpack) {}
+      pack(pack), unpack(unpack) { }
 
 private:
-  friend template<typename T>
-  Packet pack(struct Pack<T>, const T &);
+  template<typename T>
+  friend Packet pack(struct Pack<T>, const T &);
 
-  friend template<typename T>
-  void packInto(struct Pack<T>, const T &, Packet &);
+  template<typename T>
+  friend void packInto(struct Pack<T>, const T &, Packet &);
 
-  friend template<typename T>
-  T unpack(Pack<T> rules, Packet &packet);
+  template<typename T>
+  friend T unpack(Pack<T> rules, Packet &packet);
 
-  friend template<typename T>
-  void unpackInto(Pack<T> rules, Packet &packet, const T &value);
+  template<typename T>
+  friend void unpackInto(Pack<T> rules, Packet &packet, const T &value);
 
   std::function<void(Packet &, const Value &)> pack;
   std::function<void(Packet &, Value &)> unpack;
 };
 
 template<typename T>
-Packet pack(Pack<T> rules, const T & value) {
+Packet pack(Pack<T> rules, const T &value) {
   Packet packet;
   rules.pack(packet, value);
   return packet;
 }
 
 template<typename T>
-void packInto(struct Pack<T> rules, const T & value, Packet & packet) {
+void packInto(struct Pack<T> rules, const T &value, Packet &packet) {
   rules.pack(packet, value);
 }
 
@@ -67,11 +69,52 @@ void unpackInto(Pack<T> rules, Packet &packet, const T &value) {
  */
 
 /**
- * Packs a T by reading and writings its bytes, in a union.
+ * Packs a boolean in one bit. Doesn't get more space-efficent than this.
+ */
+struct BoolPack : Pack<bool> {
+  BoolPack();
+};
+
+/**
+ * Packs a std::string, using the desired type to represent its length.
+ */
+template<typename SizeT>
+struct CustomStringPack : Pack<std::string> {
+  CustomStringPack() : Pack<std::string>(
+      [](Packet &packet, const std::string &value) {
+        assert(8 ^ sizeof(SizeT) < value.length());
+        packet.writeValue<SizeT>((SizeT) value.length());
+        for (const char x : value)
+          packet.writeChar((unsigned char) x);
+      },
+      [](Packet &packet, std::string &value) {
+        value.clear();
+        SizeT size = packet.readValue<SizeT>();
+        while (size > 0) {
+          value.push_back(packet.readChar());
+          size--;
+        }
+      }) { }
+};
+
+/**
+ * Packs a std::string, representing its length in a single unsigned char,
+ * therefore cutting off more than 255 characters.
+ */
+using StringPack = CustomStringPack<unsigned char>;
+
+/**
+ * Packs a T byte-wise. Unions to the rescue!
  */
 template<typename T>
 struct BytePack : Pack<T> {
-
+  BytePack() : Pack<T>(
+      [](Packet &packet, const T &value) {
+        packet.writeValue<T>(value);
+      },
+      [](Packet &packet, T &value) {
+        value = packet.readValue<T>();
+      }) { }
 };
 
 }
