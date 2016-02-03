73e5b8bd8e23f6fbfdfce19dfe32bdc93e65ab79
Chris Gadzinski <zenmags@gmail.com>
2016-02-03 15:00:46 +0000

    so uh, no memory corruption yet

--- src/server/server.cpp
+++ src/server/server.cpp
@@ -2,12 +2,40 @@
 #include "pack/pack.h"
 #include <iostream>
 
+struct MyStruct {
+  MyStruct() { }
+  MyStruct(int x, optional<int> y) : x(x), y(y) {}
+
+  int x = 0;
+  optional<int> y{0};
+
+  void dump() {
+    std::cout << "x: ";
+    std::cout << x;
+    if (y) {
+      std::cout << ", y: ";
+      std::cout << *y;
+    }
+    std::cout << "\n";
+  }
+};
+
 int main() {
-  pk::Pack<float> floatPack = pk::BytePack<float>();
+  pk::Pack<optional<int>> optionalPack =
+      pk::OptionalPack<int>(pk::BytePack<int>());
+  pk::Pack<int> intPack = pk::BytePack<int>();
+
+  pk::Pack<MyStruct> classPack = pk::ClassPack<MyStruct>(
+      pk::MemberRule<MyStruct, int>(intPack, &MyStruct::x),
+  pk::MemberRule<MyStruct, optional<int>>(optionalPack, &MyStruct::y)
+  );
 
   pk::Packet packet;
 
-  packet = pk::pack(floatPack, 50.5f);
+  MyStruct x(24, 42);
+  x.dump();
+  packet = pack(classPack, x);
   packet.dumpBinary();
-  std::cout << pk::unpack(floatPack, packet);
+  x = unpack(classPack, packet);
+  x.dump();
 }
