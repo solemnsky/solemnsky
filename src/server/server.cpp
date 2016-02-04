4c2b691684f6302d7d92f68e780122cbb0dc6dda
MagneticDuck <zenmags@gmail.com>
2016-02-04 20:34:20 +0000

    something a bit like this, need to fix Packet with the new constant-size buffer now

--- src/server/server.cpp
+++ src/server/server.cpp
@@ -1,5 +1,6 @@
 #include "server.h"
-#include "pack/pack.h"
+
+#include "pack/telegraphy.h"
 #include "sky/delta.h"
 #include <iostream>
 
@@ -12,4 +13,12 @@ int main() {
   packet.dump();
   tuning = unpack(planeTuningPack, packet);
   std::cout << tuning.energy.recharge;
+
+  pk::Telegraph telegraph(424242);
+  telegraph.transmit(
+      pk::Transmission(packet, sf::IpAddress("192.134.329"),
+      pk::Strategy(pk::Strategy::Control::None)));
+  telegraph.receive([&](pk::Reception &&reception) {
+    tuning = unpack(planeTuningPack, reception.packet);
+  });
 }
