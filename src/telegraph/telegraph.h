ebadb03be2877dac60d6ac34081c087785af3dd9
Chris Gadzinski <zenmags@gmail.com>
2016-02-11 15:40:07 +0000

    enetpp seems to have issues building, making a simple little custom wrapper for enet itself

--- src/telegraph/telegraph.h
+++ src/telegraph/telegraph.h
@@ -5,14 +5,51 @@
 #ifndef SOLEMNSKY_TELEGRAPH_H
 #define SOLEMNSKY_TELEGRAPH_H
 
-#include "enetpp/client.h"
-#include "enetpp/server.h"
+#include "enet/enet.h"
+#include "util/types.h"
 
 namespace tg {
 
-template<typename ReceiveType, typename TransmitType>
-class Telegraph {
+namespace detail {
+static int enetUsageCount = 0;
+}
 
+/**
+ * ENet functionality can be used iff one of these is alive.
+ * We need this because of enet's global state initialization /
+ * deinitialization.
+ */
+struct UsageFlag {
+  UsageFlag();
+  ~UsageFlag();
+};
+
+/**
+ * General encapsulation of a ENetHost.
+ */
+class Host {
+ private:
+  ENetHost *host;
+  ENetEvent event;
+
+ public:
+  Host(const bool isServer,
+       const unsigned short port = 0);
+  ~Host();
+
+  // uncopyable + unassignable
+  Host(const Host &) = delete;
+  Host &operator=(const Host &) = delete;
+
+  /**
+   * API.
+   */
+  ENetPeer *connect(const std::string &address, const unsigned short port);
+  void disconnect(ENetPeer *);
+  void transmit(ENetPeer *const peer,
+                unsigned char *data, size_t size,
+                const ENetPacketFlag flag);
+  ENetEvent poll();
 };
 
 }
