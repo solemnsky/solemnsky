625dcd8c4be2ebb10b8b9648e55defc984a283b9
Chris Gadzinski <zenmags@gmail.com>
2016-03-13 16:03:58 +0000

    will work on this later today

--- src/client/multiplayer/multiplayer.cpp
+++ src/client/multiplayer/multiplayer.cpp
@@ -59,8 +59,8 @@ void Multiplayer::doExit() {
 
 void Multiplayer::tick(float delta) {
   optional<sky::ServerPacket> packet = connection.poll(delta);
-  if (connection.disconnected) {
-    quitting = true;
+  if (connection.disconnecting) {
+    if (connection.disconnected) quitting = true;
     return;
   }
 
