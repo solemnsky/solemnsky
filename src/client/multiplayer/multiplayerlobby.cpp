b5f60a08147ea4e8100b43c6fd323ce56d71c876
Chris Gadzinski <zenmags@gmail.com>
2016-04-07 21:17:27 +0100

    mistakes

--- src/client/multiplayer/multiplayerlobby.cpp
+++ src/client/multiplayer/multiplayerlobby.cpp
@@ -75,12 +75,11 @@ void MultiplayerLobby::signalRead() {
   if (chatInput.inputSignal) {
     const std::string &str = chatInput.inputSignal.get();
     if (str.size() >= 1) {
-      if (str.size() > 1 and str[0] == '/')
+      if (str.size() > 1 and str[0] == '/') {
         connection.rcon(str.substr(str.size() - 1));
-      else
+      } else {
         connection.chat(chatInput.inputSignal.get());
-        connection.transmit(
-            sky::ClientPacket::Chat(chatInput.inputSignal.get()));
+      }
     }
   }
 }
