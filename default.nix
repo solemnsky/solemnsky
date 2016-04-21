15c88721f7f4c8c630963e73d128f725d21d10d2
Chris Gadzinski <zenmags@gmail.com>
2016-04-21 07:08:09 +0000

    better nix build file

--- default.nix
+++ default.nix
@@ -3,15 +3,13 @@
 with nixpkgs;
 
 stdenv.mkDerivation {
-  patchPhase = ''
-  # rm -rf thirdparty/Box2D/Box2D/{glew,glfw,HelloWorld,Testbed}/*
-  '';
-  
   buildInputs = [cmake mesa xorg.libX11 xorg.xcbutilimage eudev libjpeg openal flac libogg freetype libvorbis glew glfw2 boost];
 
   installPhase = ''
-    mkdir $out
-    cp {libsolemnsky.so,solemnsky_client,solemnsky_server} $out
+    cmake .;
+    make solemnsky_client solemnsky_server;
+    mkdir -p $out;
+    cp solemnsky_{client,server} $out/
   '';
 
   name = "solemnsky";
