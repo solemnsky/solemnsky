e747d4ae6ec70225821a2ebe716e57e2fef04ff5
Chris Gadzinski <zenmags@gmail.com>
2016-04-23 18:43:27 +0100

    more unit tests

--- tests/skytest.cpp
+++ tests/skytest.cpp
@@ -7,6 +7,54 @@
  */
 class SkyTest: public testing::Test {
  public:
-  SkyTest() { }
+  SkyTest() :
+      arena(sky::ArenaInitializer("arena", "test1")),
+      sky(arena, sky::SkyInitializer()) { }
+
+  sky::Arena arena;
+  sky::Sky sky;
 };
+
+/**
+ * The map allocation is managed.
+ */
+TEST_F(SkyTest, MapAlloc) {
+  ASSERT_EQ(bool(sky.map), false);
+
+  arena.applyDelta(sky::ArenaDelta::Mode(sky::ArenaMode::Game));
+
+  ASSERT_EQ(bool(sky.map), true);
+
+  arena.applyDelta(sky::ArenaDelta::Mode(sky::ArenaMode::Scoring));
+
+  ASSERT_EQ(bool(sky.map), false);
+}
+
+/**
+ * We can affect the Sky, and copy the modified Sky over the network with
+ * SkyInitializer.
+ */
+TEST_F(SkyTest, InitializerTest) {
+  arena.connectPlayer("nameless plane");
+  sky::Player *player = arena.getPlayer(0);
+  player->spawn({}, {300, 300}, 0);
+
+  ASSERT_EQ(sky.getPlane(*player).isSpawned(), true);
+
+  sky::Arena remoteArena(arena.captureInitializer());
+  sky::Sky remoteSky(remoteArena, sky.captureInitializer());
+
+  ASSERT_EQ(remoteSky.getPlane(*player).isSpawned(), true);
+}
+
+/**
+ * We can propogate changes over the network with a SkyDelta.
+ */
+TEST_F(SkyTest, DeltaTest) {
+  sky::Arena remoteArena(arena.captureInitializer());
+  sky::Sky remoteSky(remoteArena, sky.captureInitializer());
+
+  // TODO: finish
+}
+
 
