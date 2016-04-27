aa5c18d6b745cb5709eada15f290dd5fa5bd9df9
Chris Gadzinski <zenmags@gmail.com>
2016-04-27 21:01:45 +0000

    trying to get things working

--- default.nix
+++ default.nix
@@ -3,15 +3,7 @@
 with nixpkgs;
 
 stdenv.mkDerivation {
-  buildInputs = [cmake mesa xorg.libX11 sfml xorg.xcbutilimage eudev libjpeg openal flac libogg freetype libvorbis glew glfw2 boost];
-
-  installPhase = ''
-    mkdir dist;
-    cd dist;
-    cmake ..;
-    mkdir -p $out;
-    cp -r ./* $out;
-  '';
+  buildInputs = [cmake eudev xorg.libX11 xorg.xcbutilimage eudev libjpeg openal flac libogg freetype libvorbis glew glfw2 boost];
 
   name = "solemnsky";
   src = ./.;
