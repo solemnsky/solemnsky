ad5f93bd323ea5744377c87a79b3a935ac24b76b
Chris Gadzinski <zenmags@gmail.com>
2016-05-20 16:42:52 +0100

    finished scoreboard and associated tests

--- tests/scoreboardtest.cpp
+++ tests/scoreboardtest.cpp
@@ -18,8 +18,45 @@ class ScoreboardTest: public testing::Test {
 };
 
 /**
- * We can alter a scoreboard and copy it with a ScoreboardInit.
+ * We can alter a scoreboard and copy it to a remote.
  */
 TEST_F(ScoreboardTest, CopyTest) {
-arena.connectPlayer("nameless guy");
+  arena.connectPlayer("nameless plane");
+  sky::Player &player1 = *arena.getPlayer(0);
+  ASSERT_EQ(scoreboard.getScore(player1).getValueAt(0), 0);
+  scoreboard.getScore(player1).setValueAt(0, 5);
+  ASSERT_EQ(scoreboard.getScore(player1).getValueAt(0), 5);
+
+  sky::Arena remoteArena(arena.captureInitializer());
+  sky::Scoreboard remoteScoreboard(remoteArena,
+                                   scoreboard.captureInitializer());
+  sky::Player &remotePlayer1 = *remoteArena.getPlayer(0);
+  ASSERT_EQ(remoteScoreboard.getFields()[1], "deaths");
+  ASSERT_EQ(remoteScoreboard.getScore(remotePlayer1).getValueAt(0), 5);
 }
+
+/**
+ * We can transmit changes to a remote.
+ */
+TEST_F(ScoreboardTest, DeltaTest) {
+  sky::Arena remoteArena(arena.captureInitializer());
+  sky::Scoreboard remoteScoreboard(remoteArena,
+                                   scoreboard.captureInitializer());
+
+  const auto delta = arena.connectPlayer("nameless plane");
+  remoteArena.applyDelta(delta);
+
+  {
+    sky::Player &player = *arena.getPlayer(0);
+    scoreboard.getScore(player).setValueAt(0, 10);
+    scoreboard.getScore(player).setValueAt(1, 5);
+  }
+
+  {
+    remoteScoreboard.applyDelta(scoreboard.collectDelta());
+    sky::Player &player = *remoteArena.getPlayer(0);
+    ASSERT_EQ(remoteScoreboard.getScore(player).getValueAt(0), 10);
+    ASSERT_EQ(remoteScoreboard.getScore(player).getValueAt(1), 5);
+  }
+}
+
