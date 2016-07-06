0ab87c3bc0e8581de7a85971cfdc216939ef84ce
Chris Gadzinski <zenmags@gmail.com>
2016-07-06 14:46:11 +0000

    starting to write zip utility

--- tests/archivetest.cpp
+++ tests/archivetest.cpp
@@ -16,6 +16,22 @@ class ArchiveTest: public testing::Test {
  * Basic operations work.
  */
 TEST_F(ArchiveTest, BasicTest) {
+  Archive archive("test.zip");
+  ASSERT_EQ(archive.isDone(), false);
 
+  archive.load();
+  archive.finishLoading(); // blocking
+
+  ASSERT_EQ(archive.isDone(), true);
+  ASSERT_EQ((bool) archive.getError(), false);
+  ASSERT_EQ((bool) archive.getResult(), true);
+
+  const auto dir = *archive.getResult();
+  ASSERT_EQ(dir.name, "test.zip");
+  ASSERT_EQ(dir.files, std::vector<std::string>({"asdf", "fdsa"}));
+  ASSERT_EQ(dir.directories.size(), size_t(1));
+
+  const auto subdir = dir.directories[0];
+  ASSERT_EQ(subdir.name, "subdirectory");
 }
 
