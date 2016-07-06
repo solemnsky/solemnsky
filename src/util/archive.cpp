4edf278cdccbacf98c44c9f092818fe254895103
Chris Gadzinski <Chris Gadzinski>
2016-07-06 14:40:03 +0100

    swapping out mingw-std-threads submodule for inline copy, to make a single change

---
+++ src/util/archive.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Chris on 06/07/2016.
+//
+
+#include "archive.hpp"
---
+++ src/util/archive.hpp
@@ -0,0 +1,8 @@
+//
+// Created by Chris on 06/07/2016.
+//
+
+#ifndef SOLEMNSKY_ARCHIVE_HPP
+#define SOLEMNSKY_ARCHIVE_HPP
+
+#endif //SOLEMNSKY_ARCHIVE_HPP
