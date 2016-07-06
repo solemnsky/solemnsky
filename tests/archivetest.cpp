8a46f1c50417a76500796373031856be3e8ec7ad
Chris Gadzinski <Chris Gadzinski>
2016-07-06 14:40:47 +0100

    archive utility and updated respective test

--- tests/archivetest.cpp
+++ tests/archivetest.cpp
@@ -1,10 +1,9 @@
 #include <gtest/gtest.h>
-#include <archive.h>
-#include <archive_entry.h>
 #include "util/printer.hpp"
+#include "util/archive.hpp"
 
 /**
- * Our small libarchive wrapper for reading .tar files 
+ * Our small 7zip wrapper for reading zip archives
  * does what one might expect it to.
  */
 class ArchiveTest: public testing::Test {
@@ -14,43 +13,9 @@ class ArchiveTest: public testing::Test {
 };
 
 /**
- * LibArchive does what I expect it to.
+ * Basic operations work.
  */
-TEST_F(ArchiveTest, LibArchiveTest) {
-  archive *archive;
-  archive_entry *entry;
-  char* buffer = new char[100];
+TEST_F(ArchiveTest, BasicTest) {
 
-  // Allocate the archive.
-  archive = archive_read_new();
-  archive_read_support_filter_all(archive);
-  archive_read_support_format_all(archive);
-
-  // Open the archive.
-  ASSERT_EQ(
-      archive_read_open_filename(archive, "tests/test.tar", 10240), 
-      ARCHIVE_OK);
-
-  // Loop over header entries.
-  while (archive_read_next_header(archive, &entry) == ARCHIVE_OK) {
-    appLog("reading: " + std::string(archive_entry_pathname(entry)));
-    size_t bufferSize, readSize = 0;
-    bool isFirstLoop = true;
-
-    // Incrementally read data.
-    for (;;) {
-      bufferSize = archive_read_data(archive, buffer, 100);
-      if (bufferSize == 0) break;
-      readSize += bufferSize;
-      if (isFirstLoop) {
-        appLog("contents of first buffer (literal): \n" + std::string(buffer, bufferSize));
-      }
-      isFirstLoop = false;
-    }
-    appLog("done! total read bytes: " + std::to_string(readSize));
-  }
-
-  // Free the archive.
-  ASSERT_EQ(archive_read_free(archive), ARCHIVE_OK);
 }
 
