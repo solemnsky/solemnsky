68022616953a8f6b96b93eccae52f3c703ad58ea
Chris Gadzinski <zenmags@gmail.com>
2016-07-07 12:13:14 +0000

    having fun with filesystem compatibility

--- tests/environmenttest.cpp
+++ tests/environmenttest.cpp
@@ -28,3 +28,48 @@ TEST_F(EnvironmentTest, NullTest) {
 
 }
 
+/**
+ * Certain issues cause non-fatal, gracefully handled errors.
+ */
+TEST_F(EnvironmentTest, ErrorTest) {
+  sky::Environment environment("does-not-exist");
+  environment.waitForLoading();
+
+  ASSERT_TRUE(environment.loadingIdle());
+  ASSERT_TRUE(environment.loadingErrored());
+  ASSERT_EQ(environment.getMap(), nullptr);
+}
+
+/**
+ * We can load environments from files.
+ */
+TEST_F(EnvironmentTest, BasicTest) {
+  sky::Environment environment("demo"); // we should have a demo.sky something
+  ASSERT_FALSE(environment.loadingIdle()); // we should be doing work!
+
+  environment.waitForLoading();
+  // this might take a second, we need to unpack the archive and load the map
+  // good thing it's asynchronous
+
+  // The map has loaded properly!
+  ASSERT_TRUE(environment.loadingIdle());
+  ASSERT_FALSE(environment.loadingErrored());
+  ASSERT_NE(environment.getMap(), nullptr);
+
+  ASSERT_EQ(environment.getMap()->getDimensions().x, 1600);
+}
+
+/**
+ * Graphics can be loaded from environment files too, when we ask for them.
+ */
+TEST_F(EnvironmentTest, GraphicsTest) {
+  // TODO: implement
+}
+
+/**
+ * We can even load scripts from environment files.
+ */
+TEST_F(EnvironmentTest, ScriptTest) {
+  // TODO: implement
+}
+
