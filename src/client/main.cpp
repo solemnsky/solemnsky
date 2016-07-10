c19c6a00dcab7d797dc1f67b8da7ce2e4b09e465
Chris Gadzinski <zenmags@gmail.com>
2016-07-10 11:35:16 +0000

    isolated and fixed issue, resolving #26

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -24,17 +24,10 @@
 int main() {
   // TODO: commandline arguments?
   // and He said,
-  
-  // ui::runSFML([](const ui::AppRefs &appState) {
-    // return std::make_unique<Client>(appState); 
-  // });
- 
-  ui::ResourceLoader loader({}, {});
-  loader.loadAllThreaded();
-  while (!loader.getHolder()) {
-    sf::sleep(sf::seconds(0.1));
-  }
-  appLog("done loading!");
-  
+
+  ui::runSFML([](const ui::AppRefs &appState) {
+    return std::make_unique<Client>(appState);
+  });
+
   // and lo, there appeared a client
 }
